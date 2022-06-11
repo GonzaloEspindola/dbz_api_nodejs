90f37be313faad141c97e506632e4466c1c97d3a
Gonzalo Espindola <espindolamgonzalo@gmail.com>
2022-06-11 06:47:38 -0300

    deploy configuration

--- libs/postgres.js
+++ libs/postgres.js
@@ -1,10 +1,16 @@
 const { Pool } = require('pg');
 const { config } = require('../config/config');
 
-//obteniendo variables de ambiente declaradas en el archivo env
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+let URI = '';
+
+if (config.isProd) {
+    URI = config.dbUrl;
+}else {
+    //obteniendo variables de ambiente declaradas en el archivo env
+    const USER = encodeURIComponent(config.dbUser);
+    const PASSWORD = encodeURIComponent(config.dbPassword);
+    URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+}
  
 //creando la pool de conexiones
 const pool = new Pool({connectionString: URI});
